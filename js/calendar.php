22c1e9fd39f249d251c34e726a1f231ba0cc8edc
fuxuqiang <fuxuqiang@outlook.com>
2016-09-17 17:33:17 +0800

    optimize

--- js/calendar.php
+++ js/calendar.php
@@ -13,23 +13,22 @@
  * $Id: calendar.php 17217 2011-01-19 06:29:08Z liubo $
 */
 
-$lang = (!empty($_GET['lang'])) ? trim($_GET['lang']) : 'zh_cn';
-
-if (!file_exists('../languages/' . $lang . '/calendar.php') || strrchr($lang,'.'))
-{
-    $lang = 'zh_cn';
+if (isset($_GET['lang']) && in_array(trim($_GET['lang']), ['zh_cn', 'en_us', 'zh_tw'])) {
+	$lang = trim($_GET['lang']);
+} else {
+	$lang = 'zh_cn';
 }
 
-require(dirname(dirname(__FILE__)) . '/data/config.php');
+require dirname(__DIR__).'/data/config.php';
 header('Content-type: application/x-javascript; charset=' . EC_CHARSET);
 
-include_once('../languages/' . $lang . '/calendar.php');
+require '../languages/'.$lang.'/calendar.php';
 
 foreach ($_LANG['calendar_lang'] AS $cal_key => $cal_data)
 {
     echo 'var ' . $cal_key . " = \"" . $cal_data . "\";\r\n";
 }
 
-include_once('./calendar/calendar.js');
+require './calendar/calendar.js';
 
 ?>
